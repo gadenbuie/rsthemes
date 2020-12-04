f852113882ba579d18d3cfff0da0be99485852db
Garrick Aden-Buie <garrick@adenbuie.com>
2020-12-04 09:20:14 -0500

    Add extract_colors() utility function

--- R/utils.R
+++ R/utils.R
@@ -64,3 +64,38 @@ ls_theme_palettes <- function(path_palettes = NULL, base16 = FALSE) {
 
 ls_sass <- function(...) fs::dir_ls(..., regexp = "[.]s[ca]ss$")
 ls_rstheme <- function(...) fs::dir_ls(..., regexp = "[.]rstheme$")
+
+extract_colors <- function(path) {
+  text <- if (length(path) == 1 && !grepl("\n", path)) {
+    readLines(path)
+  } else {
+    path
+  }
+  text <- paste(text, collapse = "\n")
+  m <- gregexpr("(#[a-fA-F0-9]+|rgba?\\([0-9,. ].+?\\))", text, perl = TRUE)
+  colors <- regmatches(text, m)[[1]]
+
+  hex <- function(x) sprintf("%02x", round(as.numeric(x)))
+  rgb <- function(r, g, b) paste0("#", hex(r), hex(g), hex(b))
+  rgba <- function(r, g, b, a) {
+    a <- as.numeric(a) * 255
+    paste0(rgb(r, g, b), hex(a))
+  }
+
+  is_rgb <- grepl("rgb\\(", colors)
+  is_rgba <- grepl("rgba\\(", colors)
+
+  colors[is_rgb] <- purrr::map_chr(colors[is_rgb], ~ eval(parse(text = .x)))
+  colors[is_rgba] <- purrr::map_chr(colors[is_rgba], function(x) {
+    if (grepl("%", x)) {
+      pct <- regmatches(x, gregexpr("[0-9.]+%", x))[[1]]
+      pct_dec <- as.numeric(sub("%", "", pct)) / 100
+      for (i in seq_along(pct)) {
+        x <- gsub(pct, pct_dec, x, fixed = TRUE)
+      }
+    }
+    eval(parse(text = x))
+  })
+
+  colors
+}
