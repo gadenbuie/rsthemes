59e68c7180900eb833ac8e88ddbfcc1bbb4867ce
Garrick Aden-Buie <garrick@adenbuie.com>
2021-04-01 10:36:15 -0400

    rebuild themes

--- inst/templates/make.R
+++ inst/templates/make.R
@@ -1,6 +1,7 @@
 devtools::load_all()
 
-r_files <- fs::dir_ls("inst/templates/", regexp = "R$")
+r_files <- fs::dir_ls(here::here("inst/templates/"), regexp = "R$")
+r_files <- r_files[!grepl("make[.]R", r_files)]
 
 purrr::walk(r_files, source)
 
