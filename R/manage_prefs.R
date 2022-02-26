83f5f55dade9f04006b5b3f6f1e7e99179d96a95
Garrick Aden-Buie <garrick@adenbuie.com>
2022-02-26 00:24:47 -0500

    Add snapshot, restore, and restore defaults

--- R/manage_prefs.R
+++ R/manage_prefs.R
@@ -31,12 +31,88 @@ rs_prefs_user_read <- function(path = NULL) {
   jsonlite::read_json(path)
 }
 
-rs_prefs_rstudio_read <- function(include_os_settings = FALSE) {
+rs_prefs_snapshot <- function(
+  name,
+  path = NULL,
+  include = NULL,
+  exclude = NULL,
+  include_os_settings = FALSE,
+  overwrite = FALSE
+) {
+  requires_rstudioapi(has_fun = "readRStudioPreference")
+  checkmate::assert_character(name, len = 1, any.missing = FALSE)
+
+  path <- path %||% rs_prefs_user_path_default()
+
+  snaps_all <- if (fs::file_exists(path)) rs_prefs_user_read(path)
+  snaps_all <- snaps_all %||% list()
+
+  if (name %in% names(snaps_all) && !isTRUE(overwrite)) {
+    cli::cli_abort("Snapshot {.field {name}} exists and {.code overwrite} is not {.code TRUE}")
+  }
+
+  prefs_snap <- rs_prefs_rstudio_read(include, exclude, include_os_settings)
+
+  snaps_all[[name]] <- prefs_snap
+  rs_prefs_user_write(snaps_all, path = path)
+}
+
+rs_prefs_restore <- function(name = NULL, path = NULL, verbose = FALSE) {
+  requires_rstudioapi(has_fun = "writeRStudioPreference")
+  snaps <- rs_prefs_user_read(path)
+
+  if (is.null(name)) {
+    cli::cli_alert_info("Available snapshots: {names(snaps)}")
+    return(invisible(names(snaps)))
+  }
+
+  checkmate::assert_character(name, len = 1, any.missing = FALSE)
+
+  if (!name %in% names(snaps)) {
+    cli::cli_abort(c(
+      "Snapshot {.field {name}} isn't the name of an available snapshot.",
+      "*" = "Snapshots: {names(snaps)}"
+    ))
+  }
+
+  rs_prefs_rstudio_write(snaps[[name]], verbose = verbose)
+}
+
+rs_prefs_restore_defaults <- function(verbose = FALSE) {
+  requires_rstudioapi(has_fun = "writeRStudioPreference")
+
+  old <- rs_prefs_rstudio_read(include_os_settings = FALSE)
+
+  defaults <- purrr::map(
+    purrr::set_names(names(old)),
+    ~ rstudio_prefs_schema[[.x]]$default
+  )
+
+  defaults <- purrr::compact(defaults)
+
+  rs_prefs_rstudio_write(defaults, verbose = verbose)
+
+  invisible(old)
+}
+
+rs_prefs_rstudio_read <- function(
+  include = NULL,
+  exclude = NULL,
+  include_os_settings = FALSE
+) {
   requires_rstudioapi(has_fun = "readRStudioPreference")
 
   missing_pref <- structure(NA, class = "missing_pref")
 
   all_pref_names <- names(rstudio_prefs_schema)
+  if (!is.null(include)) {
+    checkmate::assert_character(include, min.len = 1, any.missing = FALSE)
+    all_pref_names <- intersect(all_pref_names, include)
+  }
+  if (!is.null(exclude)) {
+    checkmate::assert_character(exclude, min.len = 1, any.missing = FALSE)
+    all_pref_names <- setdiff(all_pref_names, exclude)
+  }
   if (!isTRUE(include_os_settings)) {
     all_pref_names <- rs_prefs_remove_os_settings(all_pref_names)
   }
@@ -61,6 +137,8 @@ rs_prefs_rstudio_read <- function(include_os_settings = FALSE) {
 rs_prefs_rstudio_write <- function(prefs, verbose = FALSE) {
   requires_rstudioapi(has_fun = "writeRStudioPreference")
 
+  old <- rs_prefs_rstudio_read(include = names(prefs), include_os_settings = TRUE)
+
   updated <- 0
   for (name in names(prefs)) {
     if (!name %in% names(rstudio_prefs_schema)) {
@@ -68,11 +146,11 @@ rs_prefs_rstudio_write <- function(prefs, verbose = FALSE) {
     }
     if (verbose) id <- cli::cli_process_start("{name}")
     tryCatch({
-      rstudioapi::writeRStudioPreference(name, prefs[[name]])
+      rs_write_rstudio_preference(name, prefs[[name]])
       updated <- updated + 1
     }, error = function(err) {
         if (verbose) cli::cli_process_failed(id, "{.strong {name}} could not update preference")
-        cli::cli_alert_danger(err$message)
+        cli::cli_alert_danger("Could not update {.strong {name}}: {err$message}")
       }
     )
     if (verbose) cli::cli_process_done(id, "+ {name}", done_class = "")
@@ -83,7 +161,35 @@ rs_prefs_rstudio_write <- function(prefs, verbose = FALSE) {
   } else {
     cli::cli_alert_info("Updated {.strong {updated}} of {length(prefs)} preference{?s}")
   }
-  invisible(prefs)
+
+  invisible(old)
+}
+
+rs_write_rstudio_preference <- function(name, value, type = NULL) {
+  cast <- switch(
+    type %||% rstudio_prefs_schema[[name]][["type"]],
+    integer = as.integer,
+    real = ,
+    number = as.double,
+    string = as.character,
+    boolean = as.logical,
+    identity
+  )
+  if (identical(name, "editor_theme")) {
+    rstudioapi::applyTheme(value)
+    return()
+  }
+  tryCatch(
+    rstudioapi::writeRStudioPreference(name, cast(value)),
+    error = function(err) {
+      is_type_mismatch <- grepl("type mismatch", tolower(err$message))
+      if (!is.null(type) || !is_type_mismatch) {
+        rlang::abort(err$message)
+      }
+      expected <- sub("^.+expected <(.+?)>.+$", "\\1", tolower(err$message))
+      rs_write_rstudio_preference(name, value, type = expected)
+    }
+  )
 }
 
 rs_prefs_remove_os_settings <- function(pref_names) {
