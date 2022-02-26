1a9ea9df52c0f820ed5470950d9ff32adcaccf19
Garrick Aden-Buie <garrick@adenbuie.com>
2022-02-26 08:55:35 -0500

    start gist syncing

--- R/manage_prefs.R
+++ R/manage_prefs.R
@@ -343,3 +343,71 @@ print.rsthemes_rstudio_pref <- function(x, ...) {
 
   invisible(x)
 }
+
+gist <- function(id, file = "user-prefs.json") {
+  structure(
+    list(id = id, file = file),
+    class = "rsthemes_user_prefs_gist"
+  )
+}
+
+gist_url_html <- function(gist) {
+  stopifnot(inherits(gist, "rsthemes_user_prefs_gist"))
+  sprintf("https://gist.github.com/%s", gist$id)
+}
+
+#' @export
+print.rsthemes_user_prefs_gist <- function(x, ...) {
+  url <- gist_url_html(x)
+  cli::cli_inform("{.url {url}}")
+}
+
+gist_get <- function(gist) {
+  stopifnot(inherits(gist, "rsthemes_user_prefs_gist"))
+  tryCatch(
+    gh::gh("/gists/{gist_id}", gist_id = gist$id),
+    error = function(err) {
+      cli::cli_abort(
+        c(
+          "Could not get gist {.field {gist$id}}. Does that gist exist?",
+          "*" = "{.url {gist_url_html(gist)}}"
+        ),
+        parent = err
+      )
+    }
+  )
+}
+
+gist_get_prefs <- function(gist) {
+  stopifnot(inherits(gist, "rsthemes_user_prefs_gist"))
+  x <- gist_get(gist)
+
+  if (!gist$file %in% names(x[["files"]])) {
+    cli::cli_abort("{.file {gist$file}} is not a file in gist {.field {gist$id}}")
+  }
+
+  if (!identical(x[["files"]][[gist$file]][["type"]], "application/json")) {
+    cli::cli_abort("{.file {gist$file}} is not a JSON file in gist {.field {gist$id}}")
+  }
+
+  contents <- x[["files"]][[gist$file]][["content"]]
+  tmpfile <- fs::file_temp(ext = "json")
+  on.exit(fs::file_delete(tmpfile))
+  writeLines(contents, tmpfile)
+
+  jsonlite::read_json(tmpfile)
+}
+
+gist_set_prefs <- function(gist, prefs) {
+  stopifnot(inherits(gist, "rsthemes_user_prefs_gist"))
+  x <- gist_get(gist)
+
+  files <- list()
+  files[[gist[["file"]]]] <- jsonlite::toJSON(prefs, null = "null", auto_unbox = TRUE)
+
+  gh::gh(
+    "PATCH /gists/{gist_id}",
+    gist_id = gist$id,
+    .params = list(files = files)
+  )
+}
