822fefda824b0f653fb3a5eeab0523425877eff5
Eric Hunt <52219938+eric-hunt@users.noreply.github.com>
2024-08-26 13:27:11 -0400

    Added official palette colors

---
+++ inst/templates/everforest.R
@@ -0,0 +1,140 @@
+#' Everforest official color palette(s)
+#'
+#' These are the official colors as listed on the
+#' [Everforest palette page](https://github.com/sainnhe/everforest/blob/master/palette.md).
+#' @source https://github.com/sainnhe/everforest
+#' @name everforest_palette
+#'
+NULL
+
+#' @rdname everforest_palette
+#'
+dark_foreground <- list(
+  fg = "#d3c6aa",
+  red = "#e67e80",
+  orange = "#e69875",
+  yellow = "#dbbc7f",
+  green = "#a7c080",
+  aqua = "#83c092",
+  blue = "#7fbbb3",
+  purple = "#d699b6",
+  grey_0 = "#7a8478",
+  grey_1 = "#859289",
+  grey_2 = "#9da9a0",
+  statusline_1 = "#a7c080",
+  statusline_2 = "#d3c6aa", # unused in RStudio
+  statusline_3 = "#e67e80" # unused in RStudio
+)
+
+#' @rdname everforest_palette
+#'
+dark_background <- list(
+  hard = list(
+    bg_dim = "#1e2326",
+    bg_0 = "#272e33",
+    bg_1 = "#2e383c",
+    bg_2 = "#374145",
+    bg_3 = "#414b50",
+    bg_4 = "#495156",
+    bg_5 = "#4f5b58", # not currently used
+    bg_visual = "#4c3743",
+    bg_red = "#493b40",
+    bg_green = "#3c4841",
+    bg_blue = "#384b55",
+    bg_yellow = "#45443c"
+  ),
+  medium = list(
+    bg_dim = "#232a2e",
+    bg_0 = "#2d353b",
+    bg_1 = "#343f44",
+    bg_2 = "#3d484d",
+    bg_3 = "#475258",
+    bg_4 = "#4f585e",
+    bg_5 = "#56635f", # not currently used
+    bg_visual = "#543a48",
+    bg_red = "#514045",
+    bg_green = "#425047",
+    bg_blue = "#3a515d",
+    bg_yellow = "#4d4c43"
+  ),
+  soft = list(
+    bg_dim = "#293136",
+    bg_0 = "#333c43",
+    bg_1 = "#3a464c",
+    bg_2 = "#434f55",
+    bg_3 = "#4d5960",
+    bg_4 = "#555f66",
+    bg_5 = "#5d6b66", # not currently used
+    bg_visual = "#5c3f4f",
+    bg_red = "#59464c",
+    bg_green = "#48584e",
+    bg_blue = "#3f5865",
+    bg_yellow = "#55544a"
+  )
+)
+
+#' @rdname everforest_palette
+#'
+light_foreground <- list(
+  fg = "#5c6a72",
+  red = "#f85552",
+  orange = "#f57d26",
+  yellow = "#dfa000",
+  green = "#8da101",
+  aqua = "#35a77c",
+  blue = "#3a94c5",
+  purple = "#df69ba",
+  grey_0 = "#a6b0a0",
+  grey_1 = "#939f91",
+  grey_2 = "#829181",
+  statusline_1 = "#93b259",
+  statusline_2 = "#708089", # unused in RStudio
+  statusline_3 = "#e66868" # unused in RStudio
+)
+
+#' @rdname everforest_palette
+#'
+light_background <- list(
+  hard = list(
+    bg_dim = "#f2efdf",
+    bg_0 = "#fffbef",
+    bg_1 = "#f8f5e4",
+    bg_2 = "#f2efdf",
+    bg_3 = "#edeada",
+    bg_4 = "#e8e5d5",
+    bg_5 = "#bec5b2",# not currently used
+    bg_visual = "#f0f2d4",
+    bg_red = "#ffe7de",
+    bg_green = "#f3f5d9",
+    bg_blue = "#ecf5ed",
+    bg_yellow = "#fef2d5"
+  ),
+  medium = list(
+    bg_dim = "#efebd4",
+    bg_0 = "#fdf6e3",
+    bg_1 = "#f4f0d9",
+    bg_2 = "#efebd4",
+    bg_3 = "#e6e2cc",
+    bg_4 = "#e0dcc7",
+    bg_5 = "#bdc3af", # not currently used
+    bg_visual = "#eaedc8",
+    bg_red = "#fbe3da",
+    bg_green = "#f0f1d2",
+    bg_blue = "#e9f0e9",
+    bg_yellow = "#faedcd"
+  ),
+  soft = list(
+    bg_dim = "#e5dfc5",
+    bg_0 = "#f3ead3",
+    bg_1 = "#eae4ca",
+    bg_2 = "#e5dfc5",
+    bg_3 = "#ddd8be",
+    bg_4 = "#d8d3ba",
+    bg_5 = "#b9c0ab", # not currently used
+    bg_visual = "#e1e4bd",
+    bg_red = "#f4dbd0",
+    bg_green = "#e5e6c5",
+    bg_blue = "#e1e7dd",
+    bg_yellow = "#f1e4c5"
+  )
+)
