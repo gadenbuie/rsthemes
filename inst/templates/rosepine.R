967a8ac3baa9f06f1359512855dc23fb5fb35759
Eric Hunt <52219938+eric-hunt@users.noreply.github.com>
2024-08-26 11:06:03 -0400

    Collective terminal, code, and UI color updates from eric-hunt/rosepine.rstheme

--- inst/templates/rosepine.R
+++ inst/templates/rosepine.R
@@ -61,25 +61,30 @@ rose_pine_dawn <- list(
 
 #' Create Rosé Pine .rstheme file
 #'
-#' @param variant A string - A string defining the theme palette variant to use
-#' [Rosé Pine Variants](https://rosepinetheme.com/palette/)
-#' @param apply A Boolean - generate and install/apply the .rstheme file
-#' (TRUE), or just save the .rstheme file without applying (FALSE)
-#' @param as_sass A Boolean - save theme as .sass file and do not apply (TRUE),
+#' @param variant A string - A string defining the theme palette variant to use;
+#' must be "base", "moon", or "dawn";
+#' see [Rosé Pine Variants](https://rosepinetheme.com/palette/)
+#' @param apply A Boolean - generate and install/apply the .rstheme file (TRUE),
+#' or just save the .rstheme file without applying (FALSE); defaults to FALSE
+#' @param as_sass A Boolean - save theme as .sass file and do not apply (TRUE);
 #' defaults to FALSE
 #' @param ... Additional arguments to pass to [rsthemes::rstheme()] overriding
 #' defaults if necessary
 #'
 #' @export
 rosepine_rstheme <- function(
-    variant = NULL,
-    apply = TRUE,
+    variant = "base",
+    apply = FALSE,
     as_sass = FALSE,
-    ...
-) {
-  variant.choices <- c("Dawn", "Moon")
-  if (!is.null(variant)) variant <- tools::toTitleCase(variant)
-  stopifnot(is.null(variant) | variant %in% variant.choices)
+    ...) {
+  variant.choices <- list("base" = NULL, "moon" = "Moon", "dawn" = "Dawn")
+  stopifnot(
+    "Variant must be 'base', 'moon', or 'dawn'." =
+      tolower(variant) %in% names(variant.choices)
+  )
+  variant <- do.call(switch, c(tolower(variant), variant.choices))
+
+  theme_dark <- !identical(variant, "Dawn")
 
   rp_pal <- get(paste0(
     "rose_pine",
@@ -107,6 +112,15 @@ rosepine_rstheme <- function(
       "%)"
     )
   }
+  .transparentize <- function(nm, by = 0.25) {
+    paste0(
+      "transparentize($",
+      nm,
+      ", ",
+      by,
+      ")"
+    )
+  }
 
   theme_palette <- list(
     # UI
@@ -119,25 +133,6 @@ rosepine_rstheme <- function(
     fg = rp_pal$text,
     fg_2 = rp_pal$subtle,
     fg_3 = rp_pal$muted,
-    # Code
-    pal_comments = rp_pal$muted,
-    pal_messages = rp_pal$love,
-    pal_errors = rp_pal$love,
-    pal_operators = rp_pal$subtle,
-    pal_punctuation = rp_pal$subtle,
-    pal_variables = rp_pal$text,
-    pal_builtins = rp_pal$love,
-    pal_strings = rp_pal$gold,
-    pal_values = rp_pal$pine,
-    pal_booleans = rp_pal$rose,
-    pal_functions = rp_pal$rose,
-    pal_conditionals = rp_pal$pine,
-    pal_keywords = rp_pal$pine,
-    pal_keys = rp_pal$foam,
-    pal_tags = rp_pal$foam,
-    pal_types = rp_pal$foam,
-    pal_methods = rp_pal$iris,
-    pal_parameters = rp_pal$iris,
     # Terminal colors
     red = rp_pal$love,
     green = rp_pal$pine,
@@ -145,6 +140,14 @@ rosepine_rstheme <- function(
     blue = rp_pal$foam,
     purple = rp_pal$iris,
     cyan = rp_pal$rose,
+    white = .lighten(
+      if (theme_dark) "fg" else "bg_3",
+      if (theme_dark) 5 else 10
+    ),
+    black = .darken(
+      if (theme_dark) "bg_3" else "fg",
+      if (theme_dark) 5 else 10
+    ),
     # Color variants
     dark_red = .darken("red"),
     dark_green = .darken("green"),
@@ -157,13 +160,35 @@ rosepine_rstheme <- function(
     light_yellow = .lighten("yellow"),
     light_blue = .lighten("blue"),
     light_purple = .lighten("purple"),
-    light_cyan = .lighten("cyan")
+    light_cyan = .lighten("cyan"),
+    # Other
+    pal_cursor_line = .lighten("hl_1", 3),
+    pal_selection = .transparentize("red", 0.75),
+    # Code
+    pal_comments = "$fg_3",
+    pal_messages = "$red",
+    pal_errors = "$red",
+    pal_operators = "$fg_2",
+    pal_punctuation = "$fg_2",
+    pal_variables = "$fg",
+    pal_builtins = "$red",
+    pal_strings = "$yellow",
+    pal_values = "$purple",
+    pal_booleans = "$cyan",
+    pal_functions = "$cyan",
+    pal_conditionals = "$green",
+    pal_keywords = "$green",
+    pal_namespaces = "$blue",
+    pal_tags = "$blue",
+    pal_types = "$blue",
+    pal_methods = "$purple",
+    pal_parameters = "$purple"
   )
 
   if (as_sass) {
     theme_apply <- FALSE
     theme_as_sass <- TRUE
-    theme_path = here::here(
+    theme_path <- here::here(
       paste0(
         "rose-pine",
         if (!is.null(variant)) "-",
@@ -175,7 +200,7 @@ rosepine_rstheme <- function(
     if (apply) {
       theme_apply <- TRUE
       theme_as_sass <- FALSE
-      theme_path = here::here(
+      theme_path <- here::here(
         "inst/themes",
         paste0(
           "rose-pine",
@@ -187,7 +212,7 @@ rosepine_rstheme <- function(
     } else {
       theme_apply <- FALSE
       theme_as_sass <- FALSE
-      theme_path = here::here(
+      theme_path <- here::here(
         "inst/themes",
         paste0(
           "rose-pine",
@@ -200,115 +225,128 @@ rosepine_rstheme <- function(
   }
 
   theme_args <- list(
-    ##>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+    ## >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
     ##  THEME META                                          >>
-    ##>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
-    theme_name    = paste0("Rosé Pine", if (!is.null(variant)) " ", variant),
-    theme_dark    = !identical(variant, "Dawn"),
-    theme_flat    = TRUE,
+    ## >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+    theme_name = paste0("Rosé Pine", if (!is.null(variant)) " ", variant),
+    theme_dark = theme_dark,
+    theme_flat = TRUE,
     theme_palette = theme_palette,
     theme_as_sass = theme_as_sass,
-    theme_apply   = theme_apply,
-    theme_path    = theme_path,
+    theme_apply = theme_apply,
+    theme_path = theme_path,
     #
-    ##>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+    ## >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
     ##  UI                                                  >>
-    ##>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
-    ##-----------------------------------------
+    ## >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+    ## -----------------------------------------
     ##  RStudio                              --
-    ##-----------------------------------------
-    ui_background                             = "$bg",
-    ui_foreground                             = "$fg",
-    ui_rstudio_background                     = .darken("bg", 3),
-    ui_rstudio_foreground                     = "$fg",
-    ui_rstudio_tabs_inactive_background       = "$bg",
+    ## -----------------------------------------
+    ui_background = "$bg",
+    ui_foreground = "$fg",
+    ui_rstudio_background = .darken("bg", 3),
+    ui_rstudio_foreground = "$fg",
+    ui_rstudio_tabs_inactive_background = "$bg",
     ui_rstudio_tabs_inactive_hover_background = .lighten("bg", 2),
-    ui_rstudio_tabs_inactive_foreground       = "$fg_3",
-    ui_rstudio_tabs_active_background         = "$bg_3",
-    ui_rstudio_tabs_active_foreground         = "$fg",
+    ui_rstudio_tabs_inactive_foreground = "$fg_2",
+    ui_rstudio_tabs_active_background = "$bg_3",
+    ui_rstudio_tabs_active_foreground = "$fg",
+    ui_rstudio_scrollbar_background = "$bg_2",
+    ui_rstudio_scrollbar_handle = "$hl_2",
     #
-    ##----------------------------------------
+    ## ----------------------------------------
     ##  Interactions                        --
-    ##----------------------------------------
-    ui_cursor             = "transparentize($cyan, 0.25)",
-    ui_selection          = "transparentize($red, 0.75)",
-    ui_console_selection  = "$ui_selection",
+    ## ----------------------------------------
+    ui_cursor = "transparentize($cyan, 0.25)",
+    ui_selection = "$pal_selection",
+    ui_console_selection = "$ui_selection",
     #
-    ##----------------------------------------
+    ## ----------------------------------------
     ##  Guides                              --
-    ##----------------------------------------
-    ui_line_active                      = .lighten("hl_1", 3),
-    ui_line_active_selection            = "$ui_selection",
-    ui_bracket                          = "transparentize($hl_3, 0.1)",
-    ui_invisible                        = "$hl_2",
-    ui_margin_line                      = "$hl_2",
-    ui_gutter_background                = "$bg_2",
-    ui_gutter_foreground                = "$fg_3",
-    ui_debug_background                 = "transparentize($yellow, 0.75)",
-    ui_completions_background           = "transparentize($bg, 0.5)",
-    ui_completions_foreground           = "$fg",
-    ui_completions_border               = "$hl_3",
-    ui_completions_selected_background  = "$bg_3",
-    ui_completions_selected_foreground  = "$fg",
-    ui_fold_arrows_foreground           = "$fg",
-    ui_fold_arrows_background           = "$hl_2",
+    ## ----------------------------------------
+    ui_line_active = "$pal_cursor_line",
+    ui_line_active_selection = "$ui_selection",
+    ui_bracket = "transparentize($hl_3, 0.1)",
+    ui_invisible = "$hl_2",
+    ui_margin_line = "$hl_2",
+    ui_gutter_background = "$bg_2",
+    ui_gutter_foreground = "$fg_3",
+    ui_debug_background = "transparentize($yellow, 0.75)",
+    ui_completions_background = "transparentize($bg, 0.5)",
+    ui_completions_foreground = "$fg",
+    ui_completions_border = "$hl_3",
+    ui_completions_selected_background = "$bg_3",
+    ui_completions_selected_foreground = "$fg",
+    ui_fold_arrows_foreground = "$fg",
+    ui_fold_arrows_background = "$hl_2",
     #
-    ##>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+    ## >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
     ##  CODE                                                >>
-    ##>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
-    code_string               = "$pal_strings",
-    code_namespace            = "$pal_keys",
+    ## >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+    code_string = "$pal_strings",
+    # code_string_background     = "inherit",
+    code_namespace = "$pal_namespaces",
+    # code_namespace_background  = "inherit",
     code_namespace_font_style = "italic",
-    code_function             = "$pal_functions",
-    code_value                = "$pal_values",
-    code_comment              = "$pal_comments",
-    code_variable             = "$pal_variables",
-    code_message              = "$pal_messages",
-    code_reserved             = "$pal_keywords",
-    code_operator             = "$pal_operators",
-    code_identifier           = "$fg",
-    code_bracket              = "$hl_3",
+    code_function = "$pal_functions",
+    # code_function_background   = "inherit",
+    code_value = "$pal_values",
+    # code_value_background      = "inherit",
+    code_comment = "$pal_comments",
+    # code_comment_background    = "inherit",
+    code_variable = "$pal_variables",
+    # code_variable_background   = "inherit",
+    code_message = "$pal_messages",
+    # code_message_background    = "inherit",
+    code_reserved = "$pal_keywords",
+    # code_reserved_background   = "inherit",
+    code_operator = "$pal_operators",
+    # code_operator_background   = "inherit",
+    code_identifier = "$fg",
+    # code_identifier_background = "inherit",
+    code_bracket = "$hl_3",
+    # code_bracket_background    = "inherit",
     #
-    ##>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+    ## >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
     ##  RMD                                                >>
-    ##>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
-    rmd_chunk_background    = "$bg_2",
-    rmd_heading_foreground  = "$pal_tags",
-    rmd_chunk_header        = "$fg_2",
-    rmd_href                = "$pal_tags",
+    ## >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+    rmd_chunk_background = "$bg_2",
+    rmd_heading_foreground = "$pal_tags",
+    rmd_chunk_header = "$fg_2",
+    rmd_href = "$pal_tags",
     #
-    ##>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+    ## >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
     ##  SCSS / PARTIALS                                    >>
-    ##>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
-    ##-----------------------------------------
+    ## >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+    ## -----------------------------------------
     ##  Command palette                      --
-    ##-----------------------------------------
+    ## -----------------------------------------
     rsthemes::rstheme_command_palette(
       item_hover_background = "lighten($bg, 5%)",
       item_selected_background = "$hl_2"
     ),
     #
-    ##-----------------------------------------
+    ## -----------------------------------------
     ##  Rainbow parentheses                  --
-    ##-----------------------------------------
+    ## -----------------------------------------
     rsthemes::rstheme_rainbow_parentheses(
-      "$fg_3",
-      "$purple",
-      "$green",
-      "$red",
-      "$yellow",
-      "$blue",
-      "$fg"
+      "$fg_2",
+      "$light_red",
+      "$light_green",
+      "$light_yellow",
+      "$light_blue",
+      "$light_purple",
+      "$light_cyan"
     ),
     #
-    ##----------------------------------------
+    ## ----------------------------------------
     ##  Use large tabs                      --
-    ##----------------------------------------
+    ## ----------------------------------------
     rsthemes::rstheme_large_tabs(),
     #
-    ##----------------------------------------
+    ## ----------------------------------------
     ##  Dialog options                      --
-    ##----------------------------------------
+    ## ----------------------------------------
     rsthemes::rstheme_dialog_options(
       background = "$bg_3",
       heading_foreground = "$rmd_heading_foreground",
@@ -327,11 +365,11 @@ rosepine_rstheme <- function(
       select_background = "$bg_3"
     ),
     #
-    ##-----------------------------------------
+    ## -----------------------------------------
     ##  Terminal colors                      --
-    ##-----------------------------------------
+    ## -----------------------------------------
     rsthemes::rstheme_terminal_colors(
-      theme_dark = TRUE,
+      theme_dark = theme_dark,
       red = "$red",
       red_bright = "$light_red",
       green = "$green",
@@ -344,19 +382,17 @@ rosepine_rstheme <- function(
       cyan_bright = "$light_cyan",
       magenta = "$purple",
       magenta_bright = "$light_purple",
-      white = "$fg",
-      # white_bright = "$fg",
-      black = "$bg_3",
-      # black_bright = "$bg_3"
+      white = "$white",
+      black = "$black"
     ),
     #
-    ##-----------------------------------------
+    ## -----------------------------------------
     ##  Flat theme extras                    --
-    ##-----------------------------------------
+    ## -----------------------------------------
     # I used this snippet found in the the `rsthemes` Elm theme code at
     # https://github.com/gadenbuie/rsthemes inst/templates/elm.R
     # - add light bar, remove tab outline
-    '
+    "
     .rstudio-themes-flat .gwt-TabLayoutPanelTab-selected {
       .gwt-TabLayoutPanelTabInner .rstheme_tabLayoutCenter {
         box-shadow: 0 3px 0 $cyan inset;
@@ -368,20 +404,20 @@ rosepine_rstheme <- function(
         }
       }
     }
-    ',
+    ",
     # - dim the file icon when not selected
-    '
+    "
     .rstudio-themes-flat .gwt-TabLayoutPanelTab:not(.gwt-TabLayoutPanelTab-selected):not(:hover) .rstheme_tabLayoutCenter img {
       opacity: 0.5;
     }
-      ',
+      ",
     # - put more space between file icon and file name
-    '
+    "
     .rstudio-themes-flat .gwt-TabLayoutPanelTab .rstheme_tabLayoutCenter td:first-child > img {
       position: relative;
       left: -5px;
     }
-    ',
+    ",
     # - fix light text on light background in Update Packages dialog
     #   and dark text for some headings and help text in Options dialog
     '
@@ -425,9 +461,18 @@ rosepine_rstheme <- function(
     .ace_comment {
       font-style: italic;
     }
+    .ace_constant {
+      color: $pal_booleans
+    }
+    .ace_constant.ace_language {
+      color: $pal_booleans
+    }
+    .ace_constant.ace_numeric {
+      font-color: $pal_values
+    }
     .ace_bracket {
-      margin: -1px 0 0 0 !important;
-      padding: 0px;
+      margin: -1px 0 0 -1px !important;
+      padding: 1px;
       border: 0 !important;
       border-radius: 1;
     }
@@ -438,11 +483,17 @@ rosepine_rstheme <- function(
 }
 
 # Create sass files
-rosepine_rstheme(as_sass = TRUE)
-rosepine_rstheme(variant = "moon", as_sass = TRUE)
-rosepine_rstheme(variant = "dawn", as_sass = TRUE)
+purrr::walk(
+  c("base", "moon", "dawn"),
+  function(variant) {
+    rosepine_rstheme(variant = variant, as_sass = TRUE)
+  }
+)
 
 # Create rstheme files
-rosepine_rstheme(apply = FALSE)
-rosepine_rstheme(variant = "moon", apply = FALSE)
-rosepine_rstheme(variant = "dawn", apply = FALSE)
+purrr::walk(
+  c("base", "moon", "dawn"),
+  function(variant) {
+    rosepine_rstheme(variant = variant)
+  }
+)
